e7b3d1879c00b26773b85bf8097c704060cc4c02
RajatTrivedi29 <90779155+RajatTrivedi29@users.noreply.github.com>
2022-11-21 10:09:54 +0530

    Add files via upload

---
+++ c++/Untitled1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a = 14;
+int b = 13;
+float pi = 3.14;
+cout<<"the value of pi is : "<<pi;
+cout<<"\nhere the value of a is\n"<<a<<".\nthe value of b is\n "<<b;
+return 0;
+}
